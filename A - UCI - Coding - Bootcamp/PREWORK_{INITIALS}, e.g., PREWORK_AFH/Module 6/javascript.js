48bf551e0cf6a1e9867620d76e393f69363a7e49
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2021-11-30 12:25:20 -0800

    Add files via upload

---
+++ A - UCI - Coding - Bootcamp/PREWORK_{INITIALS}, e.g., PREWORK_AFH/Module 6/javascript.js
@@ -0,0 +1,24 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "400px"; document.getElementById("box").style.width = "400px"
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px"; document.getElementById("box").opacity = 0
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px"; document.getElementById("box").style.width = "250px"; 
+    
+    document.getElementById("box").style.backgroundColor = "orange"
+});
